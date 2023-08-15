11793852bfa731d5698e508c82793036435741b2
yazart <kur_os@yazart.ru>
2023-08-16 00:37:58 +0300

    1 file modified

--- src/example-test/component-for-test/component-for-test.component.spec.ts
+++ src/example-test/component-for-test/component-for-test.component.spec.ts
@@ -1,7 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CommonModule } from '@angular/common';
-import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentService } from './component.service';
 import { ComponentForTestComponent } from './component-for-test.component';
 
